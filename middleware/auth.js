7966403a6a0a109aa8666b310eb6c1a6862c9d43
flxxyz <i@mdzz.name>
2020-04-10 02:30:58 +0800

    fix: ERR_HTTP_HEADERS_SENT

--- middleware/auth.js
+++ middleware/auth.js
@@ -3,7 +3,7 @@ const ignoreUrl = ['/', '/login', '/link', '/link/.*']
 module.exports = function (req, res, next) {
   if (!new RegExp(`^(${ignoreUrl.join('|')})$`).test(req.url)) {
     if (typeof req.currentUser === 'undefined') {
-      res.redirect('/')
+      return res.redirect('/')
     }
   }
 
